a03c594384e6140f9c89747e53518c6fd1432290
geekflyer <c.theilemann@gmail.com>
2026-03-10 04:36:49 -0700

    bump android app version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
         applicationId = "org.cliprelay"
         minSdk = 31
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "0.1.0"
     }
 
